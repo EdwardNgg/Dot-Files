024a55cef6454f9bdd34de0b6d728623d7a4286b
Edward Ng <edwardlng@outlook.com>
2025-01-29 11:57:44 -0500

    Installation Script

---
+++ .install.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+# Install Xcode Command Line Tools
+echo "Installing Xcode Command Line Tools ..."
+xcode-select --install
+
+# Install Homebrew
+echo "Installing Homebrew ..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Tap Homebrew Formulae
+echo "Tapping Homebrew Formulae ..."
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+# Install Formulae
+echo "Installing Homebrew Formulae ..."
+brew install sketchybar
+brew install skhd
+brew install yabai
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+# Running Packages
+echo "Running Packages ..."
+brew services start sketchybar
+yabai --start-service
+skhd --start-service
